753f54cc960890f86de44d9ea155fe445bb26855
2-21python <98322884+2-21python@users.noreply.github.com>
2022-01-27 14:52:03 +0800

    Add files via upload

---
+++ 倒星星塔(10層).py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 14:49:09 2022
+
+@author: 10407
+"""
+
+print(list(range(10,0,-1)))
+for i in range(10,0,-1):
+    print('*'*i)
+   
+print('while loop')
+i = 10
+while i>0:
+    print('*'*i)
+    i = i - 1
---
+++ 倒星星塔.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 11:36:49 2022
+
+@author: 10407
+"""
+
+print(list(range(5,0,-1)))
+for i in range(5,0,-1):
+    print('*'*i)
+   
+print('while loop')
+i = 5
+while i>0:
+    print('*'*i)
+    i = i - 1
