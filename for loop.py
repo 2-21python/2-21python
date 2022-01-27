ecfdd82d4dc0f3ee21b38bf75b5bd896dd1b7a5e
2-21python <98322884+2-21python@users.noreply.github.com>
2022-01-27 14:53:06 +0800

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 11:04:38 2022
+
+@author: 10407
+"""
+
+#方法一
+for i in range(3,50,3):
+    print(i)
+
+#方法二
+for i in range(1,51):
+    if i % 3 == 0:
+        print(i)
---
+++ for迴圈.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 10:31:32 2022
+
+@author: 10407
+"""
+'''
+for index in [1,2,3,4]:
+    print(index)
+
+for letter in 'ABC':
+    print(letter)
+'''
+
+
+for i in range(5,16):
+    print(i)
