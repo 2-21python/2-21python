574efd9186029d1a704ff733ad62ef17834b577f
2-21python <98322884+2-21python@users.noreply.github.com>
2022-01-27 14:52:23 +0800

    Add files via upload

---
+++ 星星塔.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 11:24:35 2022
+
+@author: 10407
+"""
+print('for loop')
+for i in range(1,11):
+    print('*'*i)
+    
+print('while loop')
+i = 1
+while i<11:
+    print('*'*i)
+    i = i + 1
