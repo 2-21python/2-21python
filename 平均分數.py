d595d9a2e4c78e3841ce09590f7bd64ca384bd3c
2-21python <98322884+2-21python@users.noreply.github.com>
2022-02-03 21:38:41 +0800

    Add files via upload

---
+++ 平均分數.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 28 11:46:41 2022
+
+@author: 10407
+"""
+
+scores = []
+
+for i in range(5):
+    socre = int(input('input score:'))
+    scores.append(scores)
+    
+print(scores)
+
+total = 0
+total = total + i
+
+print('total:',total)
+print('mean:',total/3)
